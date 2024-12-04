a339246770e4e0a19ac8fb7711a5368216ec2188
Bruna Roberta <brunarob97@gmail.com>
2024-12-04 18:21:21 -0300

    Projeto Loja Games

---
+++ src/main/java/com/generation/lojagames/model/Produto.java
@@ -0,0 +1,81 @@
+package com.generation.lojagames.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	@Size(min = 3, max = 100, message = "O atributo deve conter no mínimo 03 e no máximo 100 caracteres")
+	private String nome;
+
+	@NotBlank(message = "O atributo descrição é obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo deve conter no mínimo 10 e no máximo 1000 caracteres")
+	private String descrição;
+
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrição() {
+		return descrição;
+	}
+
+	public void setDescrição(String descrição) {
+		this.descrição = descrição;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
